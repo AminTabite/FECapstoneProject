57aa924907e0d239a4f2168f19b4db7eb790caf3
AminTabite <Aminepicode13@gmail.com>
2025-11-13 00:21:36 +0100

    adds import bootstrap css
    .

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,7 @@
 import "./App.css";
 import RegistrationLogin from "./components/RegistrationLogin";
 import UtenteLogin from "./components/UtenteLogin.jsx";
+import "bootstrap/dist/css/bootstrap.min.css";
 function App() {
   return (
     <>
--- src/components/UtenteLogin.jsx
+++ src/components/UtenteLogin.jsx
@@ -7,14 +7,23 @@ const UtenteLogin = () => {
     <>
       <Container>
         <Row>
-          <Col xs={12} lg={6}>
+          <Col
+            xs={12}
+            lg={6}
+            className="g1 justify-content-center align-content-center bg-info">
+            <h1 className="justify-content-start align-center my-2 purple">
+              Effettua il login per accedere al sito
+            </h1>
             <FloatingLabel
               controlId="floatingInput"
               label="Email address"
-              className="mb-3">
+              className="mb-3 my-4 p-2">
               <Form.Control type="email" placeholder="name@example.com" />
             </FloatingLabel>
-            <FloatingLabel controlId="floatingPassword" label="Password">
+            <FloatingLabel
+              controlId="floatingPassword"
+              label="Password"
+              className="my-4 p-2">
               <Form.Control type="password" placeholder="Password" />
             </FloatingLabel>
           </Col>
