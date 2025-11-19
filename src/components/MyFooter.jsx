294d7dc7552f5f5b3097be865ee393723943f3cc
AminTabite <Aminepicode13@gmail.com>
2025-11-19 19:37:27 +0100

    begins footer
    .

---
+++ src/components/MyFooter.jsx
@@ -0,0 +1,14 @@
+import { Container, Col, Row } from "react-bootstrap";
+
+const MyFooter = () => {
+  return (
+    <Container>
+      <Row>
+        <Col></Col>
+      </Row>
+    </Container>
+  );
+};
+
+
+import default MyFooter;
